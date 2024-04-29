210e55c828cdfb8ba5b8bcdd099b068e54b6c152
toshiakisan1127 <toshiakisan1127@gmail.com>
2024-04-30 03:01:13 +0900

    slackでメンションされた時にOpenAIのAPIを叩くようにする

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+import re
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -13,7 +15,15 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 def handler_mention(event, say):
     user = event["user"]
     thread_ts = event["ts"]
-    say(thread_ts=thread_ts, text=f"Hello <@{user}>!")
+    message = re.sub("<@.*>", "", event["text"])
+
+    llm = ChatOpenAI(
+        model_name=os.environ.get("OPENAI_API_MODEL"),
+        temperature=os.environ.get("OPENAI_API_TEMPERATURE")
+    )
+    response = llm.predict(message)
+
+    say(thread_ts=thread_ts, text=response)
 
 
 # ソケットモードハンドラーを使ってアプリを起動
